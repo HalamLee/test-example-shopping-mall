b836b4e8c9002ab0502fe132c9ad605907d3631b
Lami <Halam.Lee.Lami@gmail.com>
2023-12-29 23:55:59 +0900

    feat: 2.2 테스트 환경과 매처

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -5,25 +5,35 @@ import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
 it('className prop으로 설정한 css class가 적용된다.', async () => {
-  // Arrange - 테스트를 위한 환경 만들기
-  // -> className을 지닌 컴포넌트 렌더링
-
-  // Act - 테스트할 동작 발생
-  // -> 렌더링에 대한 검증이기 때문에 이 단계는 생략
-  // -> 클릭이나 메서드 호출, prop 변경 등등에 대한 작업이 여기에 해당
-
-  // Assert - 올바른 동작이 실행되었는지 검증
-  // -> 렌더링 후 DOM에 해당 class가 존재하는지 검증
-
-  // render API를 호출 -> 테스트 환경의 jsDOM에 리액트 컴포넌트가 렌더링된 DOM 구조가 반영
-  // jsDOM: Node.js에서 사용하기 위해 많은 웹 표준을 순수 자바스크립트로 구현
   await render(<TextField className="my-class" />);
 
-  // vitest의 expect 함수를 사용하여 기대 결과를 검증
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
 
-  // className이란 내부 prop이나 state 값을 검증 (X)
-  // 렌더링되는 DOM 구조가 올바르게 변경되었는지 확인 (O) -> 최종적으로 사용자가 보는 결과는 DOM
-  expect(screen.getByPlaceholderText('텍스트를 입력해 주세요.')).toHaveClass(
-    'my-class',
-  );
+  screen.debug();
+
+  expect(textInput).toHaveClass('my-class');
+});
+
+// describe를 이용한 비슷한 테스트 그룹화 => 가독성이 좋아짐, 해당 그룹에만 함수를 적용시킬 수도 있음
+describe('placeholder', () => {
+  it('기본 placeholder "텍스트를 입력해 주세요."가 노출된다.', async () => {
+    // 기대결과 === 실제 결과 -> 성공
+    // 기대결과 !== 실제 결과 -> 실패
+    await render(<TextField />);
+
+    const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+    screen.debug();
+
+    expect(textInput).toBeInTheDocument();
+    // 단언(assertion) -> 테스트가 통과하기 위한 조건 -> 검증 실행
+  });
+
+  it('placeholder prop에 따라 placeholder가 변경된다.', async () => {
+    await render(<TextField placeholder="상품명을 입력해 주세요." />);
+
+    const textInput = screen.getByPlaceholderText('상품명을 입력해 주세요.');
+
+    expect(textInput).toBeInTheDocument();
+  });
 });

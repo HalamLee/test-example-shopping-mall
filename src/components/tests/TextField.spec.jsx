26affb62448efcb0d8d60e741a302beb5a742cfe
Lami <Halam.Lee.Lami@gmail.com>
2023-12-30 00:16:11 +0900

    feat: 2.3 setup과 teardown

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -4,6 +4,29 @@ import React from 'react';
 import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
+/*
+beforeEach(() => {
+  console.log('root - beforeEach');
+});
+
+beforeAll(() => {
+  console.log('root - beforeAll');
+});
+
+afterEach(() => {
+  console.log('root - afterEach');
+});
+
+afterAll(() => {
+  console.log('root - afterAll');
+});
+*/
+
+// my-class란 class가 항상 적용된 컴포넌트를 렌더링
+beforeEach(async () => {
+  await render(<TextField className="my-class" />);
+});
+
 it('className prop으로 설정한 css class가 적용된다.', async () => {
   await render(<TextField className="my-class" />);
 
@@ -14,11 +37,11 @@ it('className prop으로 설정한 css class가 적용된다.', async () => {
   expect(textInput).toHaveClass('my-class');
 });
 
-// describe를 이용한 비슷한 테스트 그룹화 => 가독성이 좋아짐, 해당 그룹에만 함수를 적용시킬 수도 있음
 describe('placeholder', () => {
+  beforeEach(() => {
+    console.log('placeholder - beforeEach');
+  });
   it('기본 placeholder "텍스트를 입력해 주세요."가 노출된다.', async () => {
-    // 기대결과 === 실제 결과 -> 성공
-    // 기대결과 !== 실제 결과 -> 실패
     await render(<TextField />);
 
     const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
@@ -26,7 +49,6 @@ describe('placeholder', () => {
     screen.debug();
 
     expect(textInput).toBeInTheDocument();
-    // 단언(assertion) -> 테스트가 통과하기 위한 조건 -> 검증 실행
   });
 
   it('placeholder prop에 따라 placeholder가 변경된다.', async () => {

bd63dc0d307db3a165dbebad4c3ee29d36288aa0
Lami <Halam.Lee.Lami@gmail.com>
2023-12-30 21:27:22 +0900

    feat: 2.4 React Testing Library와 컴포넌트 테스트

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -4,28 +4,14 @@ import React from 'react';
 import TextField from '@/components/TextField';
 import render from '@/utils/test/render';
 
-/*
-beforeEach(() => {
-  console.log('root - beforeEach');
-});
-
-beforeAll(() => {
-  console.log('root - beforeAll');
-});
-
-afterEach(() => {
-  console.log('root - afterEach');
-});
-
-afterAll(() => {
-  console.log('root - afterAll');
-});
-*/
-
-// my-class란 class가 항상 적용된 컴포넌트를 렌더링
-beforeEach(async () => {
-  await render(<TextField className="my-class" />);
-});
+/**
+ * placeholder 설정
+ * className에 따른 css class 설정
+ * 텍스트를 입력할 때마다 onChange 핸들러 호출
+ * focus 시 border 스타일 변경
+ * focus 시 onFocus 핸들러 호출
+ * Enter 키 입력 시 onEnter 핸들러 호출
+ */
 
 it('className prop으로 설정한 css class가 적용된다.', async () => {
   await render(<TextField className="my-class" />);
@@ -38,9 +24,6 @@ it('className prop으로 설정한 css class가 적용된다.', async () => {
 });
 
 describe('placeholder', () => {
-  beforeEach(() => {
-    console.log('placeholder - beforeEach');
-  });
   it('기본 placeholder "텍스트를 입력해 주세요."가 노출된다.', async () => {
     await render(<TextField />);
 
@@ -57,5 +40,62 @@ describe('placeholder', () => {
     const textInput = screen.getByPlaceholderText('상품명을 입력해 주세요.');
 
     expect(textInput).toBeInTheDocument();
+  });
+
+  it('텍스트를 입력하면 onChange prop으로 등록한 함수가 호출된다.', async () => {
+    const spy = vi.fn(); // 스파이 함수
+    // 스파이 함수: 테스트 코드에서 특정 함수가 호출되었는지, 함수의 인자로 어떤 것이 넘어왔는지 어떤 값을 반환하는지 등 다양한 값들을 저장
+    // 보통 콜백 함수나 이벤트 핸들러가 올바르게 호출되었는지 검증하고 싶을 때 사용
+
+    const { user } = await render(<TextField onChange={spy} />);
+
+    const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+    await user.type(textInput, 'test');
+
+    expect(spy).toHaveBeenCalledWith('test');
+  });
+
+  it('엔터키를 입력하면 onEnter prop으로 등록한 함수가 호출된다.', async () => {
+    const spy = vi.fn();
+
+    const { user } = await render(<TextField onEnter={spy} />);
+
+    const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+    await user.type(textInput, 'test{Enter}');
+
+    expect(spy).toHaveBeenCalledWith('test');
+  });
+
+  it('포커스가 활성화되면 onFocus prop으로 등록한 함수가 호출된다.', async () => {
+    // 포커스 활성화 조건
+    // 탭 키로 인풋 요소로 포커스 이동
+    // 인풋 요소를 클릭했을 때
+    // textInput.focus()로 직접 발생
+
+    const spy = vi.fn();
+
+    const { user } = await render(<TextField onFocus={spy} />);
+
+    const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+    await user.click(textInput);
+    // click과 연관 -> 포커스, 마우스다운, 마우스업 등..
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('포커스가 활성화되면 border 스타일이 추가된다.', async () => {
+    const { user } = await render(<TextField />);
+
+    const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+
+    await user.click(textInput);
+
+    expect(textInput).toHaveStyle({
+      borderWidth: 2,
+      borderColor: 'rgb(25, 118, 210)',
+    });
   });
 });
